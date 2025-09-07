189c99725761db9ce586e065e98885e7de0190ae
Red <redson.eric.2004@gmail.com>
2025-09-07 23:55:07 +0300

    ecezczec

--- public/a.js
+++ public/a.js
@@ -1,15 +1,28 @@
-
 (function() {
+    function getGameObject() {
+        // 1. window.game si dispo
+        if (window.game) return window.game;
+
+        
+        const board = document.querySelector(".board");
+        if (board && board.game){
+
+            console.log(board.game.getFEN())
+
+            return board.game
+        }
+
+        return null;
+    }
 
     window.addEventListener('message', (event) => {
         if (event.source !== window) return;
 
-        if (event.data && event.data.type === 'GET_FEN') {
-            const fen = window.game?.fen || "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-            const side_ = window.game?.getPlayingAs() || 1;
+        if (event.data?.type === 'GET_FEN') {
+            const game = getGameObject();
+            const fen = game?.getFEN() || "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+            const side_ = game?.getPlayingAs?.() || 1;
             window.postMessage({ type: 'FEN_RESPONSE', fen, side_ }, '*');
         }
     });
 })();
-
-
