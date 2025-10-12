1a3d0f292e632e407ac1f4914f8b802d91d8a1ee
Red <redson.eric.2004@gmail.com>
2025-10-12 10:25:38 +0300

    remove cms

--- public/background.js
+++ public/background.js
@@ -1,14 +1,11 @@
-// background.js
 let currentConfig = null;
-let currentConfig2 = null; // pour lichess
+let currentConfig2 = null; 
 const expirationDate = "2025-11-10";
 const timeAPI =
   "https://api.timezonedb.com/v2.1/list-time-zone?key=WPOK8LWQNYUI&format=json&country=FR";
 
-// Liste des tabs popup ouverts
 let popupTabs = [];
 
-// Fonction pour envoyer config à tous les tabs du site
 function sendConfigToSite(type, config, urlPattern) {
   chrome.tabs.query({ url: urlPattern }, (tabs) => {
     for (let tab of tabs) {
@@ -17,17 +14,14 @@ function sendConfigToSite(type, config, urlPattern) {
   });
 }
 
-// Listener pour les messages
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   switch (message.type) {
     case "config":
       currentConfig = message.config;
       console.log("Config Chess.com reçue :", currentConfig);
 
-      // Envoi aux tabs Chess.com
       sendConfigToSite("config", currentConfig, "*://*.chess.com/*");
 
-      // Envoi aux tabs popup ouverts
       for (let tabId of popupTabs) {
         chrome.tabs.sendMessage(tabId, { type: "config", config: currentConfig });
       }
@@ -37,10 +31,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       currentConfig2 = message.config;
       console.log("Config Lichess reçue :", currentConfig2);
 
-      // Envoi aux tabs Lichess
       sendConfigToSite("config2", currentConfig2, "*://*.lichess.org/*");
 
-      // Envoi aux tabs popup ouverts
       for (let tabId of popupTabs) {
         chrome.tabs.sendMessage(tabId, { type: "config2", config: currentConfig2 });
       }
@@ -65,18 +57,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       return true;
 
     case "FROM_CONTENT":
-      // Renvoie les coups vers la popup
       for (let tabId of popupTabs) {
         chrome.tabs.sendMessage(tabId, { type: "TO_POPUP", data: message.data });
       }
       break;
 
     case "popupReady":
-      // Le popup se signale prêt
       if (sender.tab?.id) {
         if (!popupTabs.includes(sender.tab.id)) popupTabs.push(sender.tab.id);
 
-        // Envoi immédiat des configs si disponibles
         if (currentConfig) {
           chrome.tabs.sendMessage(sender.tab.id, { type: "config", config: currentConfig });
         }
@@ -88,7 +77,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-// Création du popup fullscreen
 chrome.action.onClicked.addListener(() => {
   chrome.windows.create(
     {
@@ -102,7 +90,6 @@ chrome.action.onClicked.addListener(() => {
         const tabId = newWindow.tabs[0].id;
         if (!popupTabs.includes(tabId)) popupTabs.push(tabId);
 
-        // Envoi des configs si elles existent déjà
         if (currentConfig) {
           chrome.tabs.sendMessage(tabId, { type: "config", config: currentConfig });
         }
