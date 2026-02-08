e5073b6bd687ce56d2003823d93fee7267224450
Red-Eric <redson.eric.2004@gmail.com>
2026-02-08 12:20:04 +0300

    stockfish 18 added

--- content.js
+++ content.js
@@ -4,6 +4,7 @@ const apiExpiration =
 
 let debugEngine = false;
 
+
 function clickButtonsByText(text) {
   const buttons = Array.from(document.querySelectorAll("button"));
   const targetButtons = buttons.filter((btn) =>
@@ -84,6 +85,122 @@ async function createWorker() {
 
   return new Worker(blobUrl);
 }
+
+
+async function createWorkerStockfish() {
+  const url = `${chrome.runtime.getURL("lib/stockfish18.js")}`;
+  const blob = new Blob([`importScripts("${url}");`], {
+    type: "application/javascript",
+  });
+  const blobUrl = URL.createObjectURL(blob);
+
+  return new Worker(blobUrl);
+}
+
+class Stockfish {
+  constructor({ elo = 20, depth = 10, multipv = 5, threads = 2, hash = 128 }) {
+    this.elo = elo;
+    this.depth = depth;
+    this.multipv = multipv;
+    this.threads = threads;
+    this.hash = hash;
+    this.style = 0;
+    this.ready = this.init();
+  }
+
+  async init() {
+    this.worker = await createWorkerStockfish();
+    this.worker.postMessage("uci");
+    this.setOptions();
+  }
+
+  setOptions() {
+    // this.worker.postMessage(`setoption name Skill Level value ${this.elo}`);
+    this.worker.postMessage(`setoption name MultiPV value ${this.multipv}`);
+    this.worker.postMessage("setoption name Ponder value false");
+
+  }
+
+  updateConfig({ elo, depth, multipv, threads, hash, style }) {
+    if (elo !== undefined) this.elo = elo;
+    if (depth !== undefined) this.depth = depth;
+    if (multipv !== undefined) this.multipv = multipv;
+    if (threads !== undefined) this.threads = threads;
+    if (hash !== undefined) this.hash = hash;
+    if (style !== undefined) this.style = style;
+    this.setOptions();
+  }
+
+  async getMoves(fen, side = "white") {
+    await this.ready;
+    const sideToMove = fen.split(" ")[1];
+
+    return new Promise((resolve) => {
+      const multipvResults = new Map();
+      this.worker.postMessage("uci");
+
+      const onMessage = (event) => {
+        const msg = event.data;
+        // console.log(msg);
+        if (typeof msg !== "string") return;
+
+        if (msg.includes(`info depth ${this.depth}`)) {
+          const multipvMatch = msg.match(/multipv (\d+)/);
+          const scoreMatch = msg.match(/score (cp|mate) (-?\d+)/);
+          const pvMatch = msg.match(/pv ([a-h][1-8][a-h][1-8][qrbn]?)/);
+
+          if (multipvMatch && scoreMatch && pvMatch) {
+            const multipv = parseInt(multipvMatch[1], 10);
+            const scoreType = scoreMatch[1];
+            let scoreValueRaw = parseInt(scoreMatch[2], 10);
+
+            if (sideToMove === "b") {
+              scoreValueRaw = -scoreValueRaw;
+            }
+
+            const bestMove = pvMatch[1]; // best Move
+            let score;
+            if (scoreType === "cp") {
+              const value = +(scoreValueRaw / 100).toFixed(2);
+              score = value > 0 ? `+${value}` : `${value}`;
+            } else if (scoreType === "mate") {
+              score =
+                scoreValueRaw > 0
+                  ? `#${scoreValueRaw}`
+                  : `#-${Math.abs(scoreValueRaw)}`;
+            }
+
+            const from = bestMove.slice(0, 2);
+            const to = bestMove.slice(2, 4);
+
+            multipvResults.set(multipv, {
+              from,
+              to,
+              eval: score,
+              fen: fen,
+              side: side,
+            });
+          }
+        }
+
+        if (msg.startsWith("bestmove")) {
+          this.worker.removeEventListener("message", onMessage);
+          resolve(
+            Array.from(multipvResults.entries())
+              .sort(([a], [b]) => a - b)
+              .map(([_, val]) => val)
+          );
+        }
+      };
+
+      this.worker.addEventListener("message", onMessage);
+      this.worker.postMessage(`position fen ${fen}`);
+      this.worker.postMessage("stop");
+      this.worker.postMessage(`go depth ${this.depth}`);
+    });
+  }
+}
+
 
 class komodo {
   constructor({
@@ -380,6 +497,14 @@ const engine = new komodo({
   hash: 128,
   personality: config.style,
 });
+
+// const engine = new Stockfish({
+//   elo: config.elo,
+//   depth: config.depth,
+//   multipv: config.lines,
+//   threads: 2,
+//   hash: 128
+// })
 
 const startCheat = () => {
   if (window.location.hostname.includes("chess.com")) {
@@ -1535,7 +1660,7 @@ const startCheat = () => {
   }
 };
 
-const LOCAL_VERSION = "1.0";
+const LOCAL_VERSION = "1.1";
 
 let downloadlink = "https://www.youtube.com/@Redson_Eric";
 
